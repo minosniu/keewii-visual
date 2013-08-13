b9052dbf988ad33dfb468ad4a52d4c396f442914
KangWoo Lee <kangwool@usc.edu>
2013-08-12 17:58:22 -0700

    update pause function
    when pause, 'pause' is shown in the screen with red font.

--- src/singleTarget/saveData.clj
+++ src/singleTarget/saveData.clj
@@ -15,7 +15,7 @@
   "This function gets a path of temp folder and session number to make temp files of data files" 
   (let [sequence-index (- session-number 1)] 
     (spit (str temp-path (format "%02d" session-number) ".txt") 
-          (str "\n" ((init/randomized-sequence sequence-index) :name) "\n") :append true )))
+          (str "\n" ((init/randomized-sequence sequence-index) :name) "\n") :append false )))
 (defn save-temp-data [time EMG1 EMG2 session-number]
   (let [temp-path (str (cur_dir) "\\temp\\")] 
     (when (and (and (>= @F1 0) (<= @F1 1000 )) (and (>= @F2 200 ) (<= @F2 3000 )) (and (>= time 0.0) (<= time 5.0)))
